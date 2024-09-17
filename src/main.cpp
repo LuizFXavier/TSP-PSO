89e002eff442c1f7ca60e8722d8aee1b5ac18a3a
LuizFXavier <zap422238@gmail.com>
2024-09-17 11:36:48 -0400

    test: Removidos testes temporários

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,5 @@ using namespace std;
 
 int main(){
     
-    Particle p1, p2;
-
-    p1.solucao_atual = {0,1,2,3};
-    p2.solucao_atual = {2,1,3,0};
-
-    Velocity v = p1 - p2;
-    for(int i = 0; i < v.value.size(); i++){
-        cout << v.value[i].first << ", "<< v.value[i].second << endl;
-    }
     return EXIT_SUCCESS;
 }
