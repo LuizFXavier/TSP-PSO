2ccb3cf320bf87928da3167337f5c4c7bba3c408
LuizFXavier <zap422238@gmail.com>
2024-09-18 00:27:18 -0400

    test: Teste geral do algoritmo

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#include "Velocity.hpp"
-#include "Particle.hpp"
+#include "PSO.hpp"
 using namespace std;
 
-int main(){
+int main(int argc, char * argv[]){
+
+    if(argc < 2){
+        return EXIT_FAILURE;
+    }
     
+    PSO pso(argv[1]);
+
+    pso.executar(argv[2]);
+
+    for(int i = 0; i <= pso.nCidades; i++)
+        cout << pso.get_best().solucao_atual[i] << " ";
+    cout << endl;
+    cout << pso.get_best().best_dist;
+    cout << endl;
+
     return EXIT_SUCCESS;
 }
