79f39838700b4f8804134c3ee2364f3282d3b555
LuizFXavier <zap422238@gmail.com>
2024-10-01 21:39:37 -0400

    style: Mudança na apresentação de resultado

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,23 @@ using namespace std;
 
 int main(int argc, char * argv[]){
 
-    if(argc < 2){
+    if(argc < 1){
         return EXIT_FAILURE;
     }
     
     PSO pso(argv[1]);
 
-    pso.executar(argv[2]);
-
+    pso.executar();
+    for(int i = 0; i < pso.nParticulas; i++){
+        for(int j = 0; j <= pso.nCidades; j++){
+            cout << pso.particulas[i].solucao_atual[j] <<" ";
+        }
+        cout << ": " << pso.calcula_caminho(pso.particulas[i].solucao_atual) << "\n";
+    }
+    cout<<"\n-------------------------------------\n";
     for(int i = 0; i <= pso.nCidades; i++)
-        cout << pso.get_best().solucao_atual[i] << " ";
+        cout << pso.get_best().solucao_atual[i]<< " ";
+    
     cout << endl;
     cout << pso.get_best().best_dist;
     cout << endl;
